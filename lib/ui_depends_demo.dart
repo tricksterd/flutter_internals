5c521efc3487c86d836e11d0e57fb4f10a7ae0ad
tricksterd <kozavkolena@gmail.com>
2024-08-11 16:32:50 +0300

    refactored code

--- lib/ui_depends_demo.dart
+++ lib/ui_depends_demo.dart
@@ -1,67 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:internals/demo_buttons.dart';
 
-class UIUpdatesDemo extends StatefulWidget {
+class UIUpdatesDemo extends StatelessWidget {
   const UIUpdatesDemo({super.key});
-
-  @override
-  StatefulElement createElement() {
-    print('UIUpdateDemo CREATEELEMENT called');
-    return super.createElement();
-  }
-
-  @override
-  State<UIUpdatesDemo> createState() => _UIUpdatesDemoState();
-}
-
-class _UIUpdatesDemoState extends State<UIUpdatesDemo> {
-  bool _isUnderstood = false;
 
   @override
   Widget build(BuildContext context) {
     print('UIUpdateDemo BUILD called');
 
-    return Padding(
-      padding: const EdgeInsets.all(8),
+    return const Padding(
+      padding: EdgeInsets.all(8),
       child: Center(
           child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Text(
+          Text(
             'Every flutter developer should have a basic understanding of Flutter\'s internals',
             textAlign: TextAlign.center,
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          const SizedBox(
+          SizedBox(
             height: 16,
           ),
-          const Text(
+          Text(
             'Do you understand how flutter updates UI?',
             textAlign: TextAlign.center,
           ),
-          const SizedBox(
+          SizedBox(
             height: 24,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = false;
-                    });
-                  },
-                  child: const Text('No')),
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = true;
-                    });
-                  },
-                  child: const Text('Yes'))
-            ],
-          ),
-          if (_isUnderstood) const Text('Awesome!')
+          DemoButtons(),
         ],
       )),
     );
